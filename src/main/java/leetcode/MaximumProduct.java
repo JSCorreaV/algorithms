32a380de09a1271691a02dd62e27f791dfd6b806
Juan Correa <Juan.Correa@endava.com>
2020-07-22 13:52:17 -0500

    Adding javadoc

--- src/main/java/leetcode/MaximumProduct.java
+++ src/main/java/leetcode/MaximumProduct.java
@@ -1,22 +1,31 @@
 package leetcode;
 
 public class MaximumProduct {
+
+    /**
+     * Given the array of integers nums,
+     * it will choose two different indices i and j of that array.
+     * and return the maximum value of (nums[i]-1)*(nums[j]-1).
+     *
+     * @param nums array of integers
+     * @return maximum value of (nums[i]-1)*(nums[j]-1)
+     */
     public static int maxProduct(int[] nums) {
         int first = nums[0];
         int second = 0;
-        for (int n : nums){
-            if (first <n){
-                second=first;
-                first=n;
-            }else if(second<n){
-                second=n;
+        for (int n : nums) {
+            if (first < n) {
+                second = first;
+                first = n;
+            } else if (second < n) {
+                second = n;
             }
         }
-        return (first-1)*(second-1);
+        return (first - 1) * (second - 1);
     }
 
-    public static void main(String[] args){
-        int[] nums = {3,4,5,2};
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 5, 2};
         System.out.println(maxProduct(nums));
     }
 
